0998def39aae25c0fc36706d97d3ef898c205828
Eli Treuherz <eli@treuherz.co.uk>
2021-02-03 23:28:43 +0000

    2 Output an Image

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,38 @@
-fn main() {}
+use std::io::Write;
+
+fn main() -> std::io::Result<()> {
+    let mut out = std::io::stdout();
+    let mut err = std::io::stderr();
+
+    const WIDTH: i32 = 256;
+    const HEIGHT: i32 = 256;
+
+    writeln!(out, "P3")?;
+    writeln!(out, "{} {}", WIDTH, HEIGHT)?;
+    writeln!(out, "255")?;
+
+    for j in (0..HEIGHT).rev() {
+        write!(err, "\rLines remaining: {:4}", j)?;
+        err.flush()?;
+        for i in 0..WIDTH {
+            let (r, g, b) = (
+                i as f64 / (WIDTH - 1) as f64,
+                j as f64 / (HEIGHT - 1) as f64,
+                0.25,
+            );
+
+            let coeff = 255.0;
+            let (ir, ig, ib) = (
+                (coeff * r) as i32,
+                (coeff * g) as i32,
+                (coeff * b) as i32,
+            );
+
+            writeln!(out, "{} {} {}", ir, ig, ib)?;
+        }
+    }
+
+    write!(err, "\nDone\n")?;
+
+    Ok(())
+}
